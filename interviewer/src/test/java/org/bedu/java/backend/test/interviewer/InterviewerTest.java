0408aadb164e1aa2340d0c7a754e866c3c5f8cb3
rrios62 <118863866+rrios62@users.noreply.github.com>
2023-06-16 10:28:04 -0600

    Postwork Fase 3 Módulo 2
    Postwork de la Fase 3, Modulo 2, Sesión 1

---
+++ interviewer/src/test/java/org/bedu/java/backend/test/interviewer/InterviewerTest.java
@@ -0,0 +1,87 @@
+package org.bedu.java.backend.test.interviewer;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InterviewerTest {
+    static String existingInterviewerName = "First";
+    static String existingInterviewerLastName = "User";
+    static String existingInterviewerEmail =  "first@email.com";
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        Interviewer.data = new ArrayList<>();
+
+        // We insert a user for testing delete and save
+        Interviewer.data.add(new Interviewer(
+                existingInterviewerName,
+                existingInterviewerLastName,
+                existingInterviewerEmail,
+                true
+        ));
+    }
+
+    @Test
+    void add() {
+        Interviewer interviewer = new Interviewer(
+                "Test",
+                "User",
+                "any@email.com",
+                true
+        );
+
+        interviewer.add();
+
+        int expectedId = Interviewer.data.size();
+        assertEquals(
+                expectedId,
+                interviewer.id,
+                "Interviewer ID should be the new List's size"
+        );
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getByEmail() {
+        Interviewer result = Interviewer.getByEmail(existingInterviewerEmail);
+
+        assertNotNull(result, "Interviewer should be found");
+        assertEquals(
+                existingInterviewerName,
+                result.name,
+                "Unexpected Interviewer Name"
+        );
+        assertEquals(
+                existingInterviewerLastName,
+                result.lastName,
+                "Unexpected Interviewer Last Name"
+        );
+    }
+
+    @Test
+    void testToString() {
+        String nonExistingEmail = "nonexisting@email.com";
+
+        Interviewer result = Interviewer.getByEmail(nonExistingEmail);
+
+        assertNull(result, "Interviewer should not be found");
+    }
+
+    @Test
+    @DisplayName("Prueba por No existe email")
+    public void getByNonExistingEmail() {
+        String nonExistingEmail = "nonexisting@email.com";
+
+        Interviewer result = Interviewer.getByEmail(nonExistingEmail);
+
+        assertNull(result, "Interviewer should not be found");
+    }
+}
